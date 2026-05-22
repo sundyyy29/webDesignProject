024213afdae8ae95dd2d7d1cc7bfa3cf855a594c
sundyyy29 <sandipugurung123@gmail.com>
2026-05-22 16:15:33 +0545

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const reserveButton = document.querySelector(".hero button");
-
-reserveButton.addEventListener("click", function () {
-
-    alert("Thank you for choosing Sakura Sushi! Your reservation request has been received.");
-
-});
